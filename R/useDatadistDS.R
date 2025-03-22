14068a8cb39e079068cebffc3df8718f48748f0d
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-03-22 16:16:00 +0100

    quote

--- R/useDatadistDS.R
+++ R/useDatadistDS.R
@@ -17,7 +17,7 @@ useDatadistDS <- function(datadist = NULL) {
     stop("The provided object is not a datadist object", call. = FALSE)
   }
 
-  eval(parse(text = paste0("options(datadist = ", datadist, ")")), envir = parent.frame())
+  eval(parse(text = paste0("options(datadist = '", datadist, "')")), envir = parent.frame())
 
   return(TRUE)
 }
